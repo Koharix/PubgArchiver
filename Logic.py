3a4d71c5d036d00cf23e16d7cfab01faca56db1b
Koharix <dgevargizian@hotmail.com>
2018-09-02 11:44:54 -0500

    Added PlayerMatchStats PMS class

--- Logic.py
+++ Logic.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import io
+import PMS
 
 apiKey = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJiNjEwODgyMC04ZjZhLTAxMzYtM2JkMi0xN2NlYWJkMDhhYTkiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTM1NzMzNzIyLCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6ImtvaGFyaXgtZGF0YSJ9.dGsB2H8IVJjQTc8x5vL4BTTIdffe9rj3ld3DUUTlvhg '
 headers = {'Authorization':'Bearer '+apiKey, 'Accept':'application/vnd.api+json'}
@@ -8,15 +9,15 @@ burl = 'https://api.pubg.com/shards/pc-na/'
 KId = 'account.c4e1432d27bd4d9296b7ad058c49172a'
 MId = 'account.c8335a1816ee452ea757695d0b811259'
 
-def getPlayerId(player):
+def getPlayerInfo(player):
     rurl = 'players?filter[playerNames]=' + player
-    r = requests.get(burl+rurl,headers=headers)
+    r = requests.get(burl + rurl, headers=headers)
     #print(r)
     #print(r.text)
     return r
 
 def getMatchId(player):
-    data = getPlayerId(player).text
+    data = getPlayerInfo(player).text
     data = json.load(io.StringIO((data)))
     matchId = data["data"][0]["relationships"]["matches"]["data"][0]["id"]
     print(matchId)
@@ -39,7 +40,12 @@ def getPlayerMatchStats(player):
             if x["attributes"]["stats"]["playerId"] == KId:
                 playerStats = x["attributes"]["stats"]
                 print(playerStats)
+                print(json.dumps(playerStats, indent=4, sort_keys=True))
+                print(playerStats["assists"])
                 return playerStats
         except KeyError:
             doNothing = 0
 
+def storePMSintoPMS(player):
+    playerstats = getPlayerMatchStats(player)
+    pms = PMS.PMS(playerstats)
---
+++ PMS.py
@@ -0,0 +1,35 @@
+
+
+class PMS:
+
+
+    DBNO
+    assists
+    boosts
+    damgeDealt
+    deatType
+    headshotKills
+    heals
+    killPlace
+    killPoints
+    killPointsDelta
+    killStreaks
+    kills
+    lastKillPoints
+    lastWinPoints
+    longestKill
+    mostDamage
+    name
+    playerId
+    revives
+    rideDistance
+    roadKills
+    swimDistance
+    teamKills
+    timeSurvived
+    vehicleDestroys
+    walkDistance
+    weaponsAcquired
+    winPlace
+    winPoints
+    winPointsDelta
