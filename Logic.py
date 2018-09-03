0355e1b920f87ef58a6bcb843200767a24bfab45
Koharix <dgevargizian@hotmail.com>
2018-09-02 19:10:47 -0500

    cleanup

--- Logic.py
+++ Logic.py
@@ -5,15 +5,13 @@ import PMS
 import datetime
 
 #google sheets
-ClientId = '450833891653-adbtqcc41jeovp2sg75gfqckc1k1s8ij.apps.googleusercontent.com'
-ClientId = ' 450833891653-adbtqcc41jeovp2sg75gfqckc1k1s8ij.apps.googleusercontent.com S'
-CilentSecret = ' r6LDwPRUe0ISwAMDPT2tQimJ '
+#ClientId = '450833891653-adbtqcc41jeovp2sg75gfqckc1k1s8ij.apps.googleusercontent.com'
+#ClientId = ' 450833891653-adbtqcc41jeovp2sg75gfqckc1k1s8ij.apps.googleusercontent.com S'
+#CilentSecret = ' r6LDwPRUe0ISwAMDPT2tQimJ '
 
 apiKey = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJiNjEwODgyMC04ZjZhLTAxMzYtM2JkMi0xN2NlYWJkMDhhYTkiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTM1NzMzNzIyLCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6ImtvaGFyaXgtZGF0YSJ9.dGsB2H8IVJjQTc8x5vL4BTTIdffe9rj3ld3DUUTlvhg '
 headers = {'Authorization':'Bearer ' + apiKey, 'Accept':'application/vnd.api+json'}
 burl = 'https://api.pubg.com/shards/pc-na/'
-KId = 'account.c4e1432d27bd4d9296b7ad058c49172a'
-MId = 'account.c8335a1816ee452ea757695d0b811259'
 pms = PMS.PMS()
 
 def getPlayerRecentMatchStat(player):
@@ -31,10 +29,7 @@ def getPlayerInfo(player):
     return playerInfo
 
 def getRecentMatchId(playerInfo):
-    #print(json.dumps(playerInfo["data"][0]["relationships"]["matches"]["data"][0]["id"], indent=4, sort_keys=True))
-    matchId = str(playerInfo["data"][0]["relationships"]["matches"]["data"][0]["id"])
-    return matchId
-    #return playerInfo["data"][0]["relationships"]["matches"]["data"][0]["id"]
+    return playerInfo["data"][0]["relationships"]["matches"]["data"][0]["id"]
 
 #returns most recent match information of player
 def getMatchStats(matchId):
