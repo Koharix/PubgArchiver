a21994b584ad777597ee569f163bd7932290d8d1
Koharix <dgevargizian@hotmail.com>
2018-09-27 20:32:34 -0500

    god is dead and we killed him

--- Main.py
+++ Main.py
@@ -1,12 +1,5 @@
 import CombinedLogic
-# #pms = PubgLogic.getPlayerRecentMatchStat(player)
-# #SheetsLogic.setRecentMatchId(pms)
-# # pms = PubgLogic.getPlayerRecentMatchStat('Koharix')
-# # SheetsLogic.appendPms(pms)
-# # print(SheetsLogic.getMostRecentMatchId())
-# PubgLogic.printJson(PubgLogic.getMatchHistory(PubgLogic.getPlayerInfo(player)))
-# recentStoredMatchId = SheetsLogic.getRecentMatchId()
-# array = PubgLogic.getUnstoredMatchIds(recentStoredMatchId)
-# print(array)
+
+
 cl = CombinedLogic.CombinedLogic()
 cl.storeRecentMatchs()
